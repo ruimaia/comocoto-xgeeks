858a750b8f730c1bdbaec8550ddc8a1befe1b796
Samuel Arcadinho <ssam_dav@hotmail.com>
2024-11-23 13:54:33 +0000

    Save in S3

--- scripts/generate_syntetic_data.py
+++ scripts/generate_syntetic_data.py
@@ -18,8 +18,13 @@ JINJA_ENV = Environment(loader=FileSystemLoader(ROOT_DIR / "prompts/"))
 
 load_dotenv()
 region = os.getenv("AWS_DEFAULT_REGION")
+BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+
+
 model_id = "mistral.mistral-large-2407-v1:0"
 bedrock_client = boto3.client(service_name='bedrock-runtime', region_name=region)
+s3_client = boto3.resource("s3")
+
 inference_config = {
     "maxTokens": 1028,
     "temperature": 1.0,
@@ -56,7 +61,6 @@ def generate_syntetic_data(
     prompt_name:str, ndatapoints: int, prompt_parameters: dict, output_file: str, required_fields: list[str]    
 ) -> list[dict[str, Any]]:
     prompt = JINJA_ENV.get_template(prompt_name)
-    output_file = ROOT_DIR / output_file
     with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
         futures = [
             executor.submit(
@@ -69,8 +73,13 @@ def generate_syntetic_data(
         dataset = [future.result() for future in tqdm(concurrent.futures.as_completed(futures), total=ndatapoints)]
         
     dataset = [datapoint for datapoint in dataset if datapoint]
-    with open(output_file, "w") as f:
-        json.dump(dataset, f)
+    
+    s3object = s3_client.Object(BUCKET_NAME, output_file)
+    s3object.put(
+        Body=(bytes(json.dumps(dataset).encode('UTF-8')))
+    )
+    # with open(output_file, "w") as f:
+    #     json.dump(dataset, f)
 
 if __name__ == "__main__":
     CLI(generate_syntetic_data)
--- src/comocoto_xgeeks/app.py
+++ src/comocoto_xgeeks/app.py
@@ -1,20 +1,30 @@
 from pathlib import Path
 from typing import Any
+import boto3
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
 from fasthx import hx, page, Jinja
+from dotenv import load_dotenv
 
+import os
 import json
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 TEMPLATES = Jinja2Templates(directory=ROOT_DIR / "app")
 jinja = Jinja(TEMPLATES)
 
-with open(ROOT_DIR / "data" / "historic_data.json") as f:
-    DATA = json.load(f)
+load_dotenv()
+BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+S3_CLIENT = boto3.resource("s3")
 
 # Create the app.
 app = FastAPI()
+
+def _fetch_data() -> list[str, Any]:
+    content_object = S3_CLIENT.Object(BUCKET_NAME, 'historic_data.json')
+    file_content = content_object.get()['Body'].read().decode('utf-8')
+    json_content = json.loads(file_content)
+    return json_content
 
 def render_index(result: list[dict[str, str]], *, context: dict[str, Any], request: Request) -> str:
     html = TEMPLATES.get_template("index.html").render()
@@ -29,4 +39,5 @@ def index() -> None:
 @app.get("/fetch-data")
 @jinja.hx("historical_data_list.html")
 def fetch_data() -> str:
-    return {"historical_data": DATA}
+    data = _fetch_data()
+    return {"historical_data": data}
