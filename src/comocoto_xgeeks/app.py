c4eb3363cd7a01fb7f92a574b76230bd983394eb
root <root@LAPTOP-7D8H7L2G.localdomain>
2024-11-23 14:27:12 +0000

    add separate css file and loading animation in main button

--- src/comocoto_xgeeks/app.py
+++ src/comocoto_xgeeks/app.py
@@ -3,6 +3,7 @@ from typing import Any
 import boto3
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from fasthx import hx, page, Jinja
 from dotenv import load_dotenv
 
@@ -20,6 +21,12 @@ S3_CLIENT = boto3.resource("s3")
 # Create the app.
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory=ROOT_DIR / "static"),
+    name="static",
+)
+
 def _fetch_data() -> list[str, Any]:
     content_object = S3_CLIENT.Object(BUCKET_NAME, 'historic_data.json')
     file_content = content_object.get()['Body'].read().decode('utf-8')
@@ -27,7 +34,9 @@ def _fetch_data() -> list[str, Any]:
     return json_content
 
 def render_index(result: list[dict[str, str]], *, context: dict[str, Any], request: Request) -> str:
-    html = TEMPLATES.get_template("index.html").render()
+    html = TEMPLATES.TemplateResponse(
+        "index.html", {"request": request}
+    )
     return html
         
         
